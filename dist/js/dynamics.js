d442e145a5f90c70dd24f553e3cd39ebb5e0a754
Mnemba Chambuya <karimnembac@gmail.com>
2022-02-02 13:22:13 +0300

    add styles, markup for dynamic more button

--- dist/js/dynamics.js
+++ dist/js/dynamics.js
@@ -37,34 +37,83 @@ const comedians = [
 ]
 
 let comediansCards = document.querySelector('#comedians-cards');
+let comediansNumbertoShow = 2;
+let nextStartIdx = 0;
 
-for(let i=0; i<comedians.length; i+=1) {
-    comedian = comedians[i];
-    let card = document.createElement('section');
-    card.classList.add('card');
+// Takes number of comedians to show, and where to start in list and adds card ?// markup for each comedian in the list
+function showComedians(howMany=comedians.length, startFrom=0) {
+    console.log('howmany: ', howMany, ': startfrom ', startFrom);
+    for(let i=startFrom; i<howMany; i+=1) {
+        comedian = comedians[i];
+        let card = document.createElement('section');
+        card.classList.add('card');
+    
+        // The image element
+        let pic = document.createElement('img');
+        pic.src = comedian.imgSrc;
+        pic.alt = comedian.imgAlt;
+        card.appendChild(pic);
+    
+        // the div element
+        let insideDiv = document.createElement('div');
+    
+        let name = document.createElement('h3');
+        name.innerText = comedian.name;
+        insideDiv.appendChild(name);
+    
+        let biography = document.createElement('h4');
+        biography.innerText = comedian.biography;
+        insideDiv.appendChild(biography);
+    
+        let description = document.createElement('p');
+        description.innerText = comedian.description;
+        insideDiv.appendChild(description);
+    
+        card.appendChild(insideDiv);
+    
+        comediansCards.appendChild(card);
+    }
+    comediansNumbertoShow = comedians.length - howMany;
+    nextStartIdx = howMany
 
-    // The image element
-    let pic = document.createElement('img');
-    pic.src = comedian.imgSrc;
-    pic.alt = comedian.imgAlt;
-    card.appendChild(pic);
-
-    // the div element
-    let insideDiv = document.createElement('div');
-
-    let name = document.createElement('h3');
-    name.innerText = comedian.name;
-    insideDiv.appendChild(name);
-
-    let biography = document.createElement('h4');
-    biography.innerText = comedian.biography;
-    insideDiv.appendChild(biography);
-
-    let description = document.createElement('p');
-    description.innerText = comedian.description;
-    insideDiv.appendChild(description);
-
-    card.appendChild(insideDiv);
-
-    comediansCards.appendChild(card);
 }
+
+// test width of window and show all comedians if widht >= 768px otherwise load only two comedians
+function displayAllComedians() {
+    if(window.innerWidth >= 768) {
+        showComedians(undefined, 0);
+    } else {
+        showComedians(comediansNumbertoShow, nextStartIdx);
+    }
+}
+
+displayAllComedians();
+
+
+
+// For when the 'MORE' button is clicked
+let moreBtn = document.querySelector('#load-more-btn');
+let lessBtn = document.querySelector('#load-less-btn');
+
+moreBtn.addEventListener('click', ()=>{
+    console.log('INSIDE.............');
+    showComedians(undefined, nextStartIdx);
+    moreBtn.classList.remove('show-inline-block')
+    moreBtn.classList.add('hide-display');
+    lessBtn.classList.remove('hide-display');
+    lessBtn.classList.add('show-inline-block');
+});
+
+lessBtn.addEventListener('click', ()=>{
+    comediansCards.innerHTML = '';
+    comediansNumbertoShow = 2;
+    nextStartIdx = 0;
+    showComedians(comediansNumbertoShow, nextStartIdx);
+    moreBtn.classList.remove('hide-display');
+    moreBtn.classList.add('show-inline-block');
+    lessBtn.classList.remove('show-inline-block');
+    lessBtn.classList.add('hide-display');
+
+})
+
+
